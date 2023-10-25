206275038045584a84e2a70574affeec467104d4
Ujjwal Kumar Agrawal <ujjwalagrawal29102001@gmail.com>
2023-10-25 22:45:23 +0530

    URL shortner Created

---
+++ ReadME.md
@@ -0,0 +1,9 @@
+- URL Shortner
+It is a url shortner web applications
+To use it you have to fork all these code and then get it in your local system
+Install MongoDB and NodeJS from browser
+Then install all npm packages required like express, mongoose, nodemon, nanoid and ejs
+type <h5>npm start</h5> on terminal and then go to <h5>localhost:3001</h5> on browser
+type the url and and get a unique code
+<h6>localhost:3001/unique_id</h6>
+replace your unique id instead of unique_id and see the magic, you will go to your url
---
+++ index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { nanoid } from 'nanoid';
+
+const app = express();
+const PORT = 3001;
+
+mongoose.connect("mongodb://0.0.0.0:27017/lalu", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Mongoose is connected");
+}).catch(() => {
+    console.log("Mongoose is not connected");
+});
+
+const urlSchema = new mongoose.Schema({
+    originalURL: { type: String, unique: true },
+    shortURL: String,
+});
+
+const URL = mongoose.model('url', urlSchema);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { shortURL: '' });
+});
+
+app.post('/shorten', async (req, res) => {
+    const { originalURL } = req.body;
+
+    try {
+        const url = await URL.create({ originalURL: "http://" + originalURL, shortURL: nanoid(10) });
+        res.json({ shortURL: url.shortURL });
+    } catch (error) {
+        if (error.code === 11000) {
+            // Handle duplicate key error (originalURL already exists)
+            const existingURL = await URL.findOne({ originalURL: "http://" + originalURL });
+            res.json({ shortURL: existingURL.shortURL });
+        } else {
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+});
+
+app.get('/:shortURL', async (req, res) => {
+    const { shortURL } = req.params;
+    const url = await URL.findOne({ shortURL });
+    if (url) {
+        res.redirect(url.originalURL);
+    } else {
+        res.status(404).json({ error: 'URL not found' });
+    }
+});
+
+
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
